f6a1e4d9bcbb21646daa3bd4cc801aa76a691e2d
Harini <144316443+HariniRajendrakumar@users.noreply.github.com>
2026-04-15 14:52:26 +0530

    Add files via upload

---
+++ chart.php
@@ -0,0 +1,56 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "chart";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Close connection
+$conn->close();
+?>
+
+<html>
+<head>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+var data = google.visualization.arrayToDataTable([
+['Players Name', 'No.of.wickets'],
+<?php
+$sql = "SELECT * FROM contribution";
+$fire = mysqli_query($con,$sql);
+
+while ($result = mysqli_fetch_assoc($fire)) {
+echo"['".$result['players']."',".$result['wickets']."],";
+}
+
+?>
+]);
+
+var options = {
+title: 'PLAYERS CONTRIBUTION'
+};
+
+var chart = new
+google.visualization.PieChart(document.getElementById('piechart'));
+
+chart.draw(data, options);
+}
+</script>
+</head>
+<body>
+<div id="piechart" style="width: 900px; height: 500px;"></div>
+</body>
+</html>
---
+++ webhook.php
@@ -0,0 +1,49 @@
+<?php
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!$data) {
+    http_response_code(400);
+    echo "No data received";
+    exit;
+}
+
+$repo = $data['repository']['full_name'];
+$user = $data['pusher']['name'];
+$message = $data['head_commit']['message'];
+
+$telegramToken = "YOUR_BOT_TOKEN";
+$chatId = "YOUR_CHAT_ID";
+
+$text = "GitHub Update Alert\n\n";
+$text .= "Repository: " . $repo . "\n";
+$text .= "Modified By: " . $user . "\n";
+$text .= "Commit Message: " . $message;
+
+$url = "https://api.telegram.org/bot" . $telegramToken . "/sendMessage";
+
+$params = [
+    'chat_id' => $chatId,
+    'text' => $text
+];
+
+$options = [
+    'http' => [
+        'method'  => 'POST',
+        'header'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+        'content' => http_build_query($params)
+    ]
+];
+
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+if ($result === FALSE) {
+    http_response_code(500);
+    echo "Failed to send Telegram notification";
+} else {
+    http_response_code(200);
+    echo "Notification Sent Successfully";
+}
+
+?>
